0bc95e15876cd586fca9bb11fe2370ddd29403c1
Bardoso244 <bart.rosiecki@gmail.com>
2025-01-13 20:16:39 +0000

    CloudFront code added

--- terraform-nextjs/main.tf
+++ terraform-nextjs/main.tf
@@ -1,3 +1,41 @@
 provider "aws" {
   region = "eu-west-2"
 }
+
+resource "aws_cloudfront_distribution" "cdf1" {
+  origin {
+    domain_name = "bartman-websiteproject.s3-website.eu-west-2.amazonaws.com"
+    origin_id   = "S3-bartman-websiteproject"
+  }
+
+  enabled             = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    target_origin_id       = "S3-bartman-websiteproject"
+    viewer_protocol_policy = "redirect-to-https"
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+
+    min_ttl     = 0
+    default_ttl = 3600
+    max_ttl     = 86400
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+}
