6ce3f9611ba417f66fee4d5f92a2de7120222d26
Bardoso244 <bart.rosiecki@gmail.com>
2025-01-19 22:19:28 +0000

    All tf code for the website, s3 and cf

--- terraform-nextjs/main.tf
+++ terraform-nextjs/main.tf
@@ -46,7 +46,7 @@ resource "aws_s3_bucket_acl" "nextjs_bucket_acl" {
 
 # Bucket policy - allowing public read on the bucket
 
-resource "aws_s3_bucket_policy" "nextjs_bucket_policy_public_read" {
+resource "aws_s3_bucket_policy" "nextjs_bucket_policy" {
   bucket = aws_s3_bucket.nextjs-website-bucket.id
 
   policy = jsonencode({
@@ -57,35 +57,51 @@ resource "aws_s3_bucket_policy" "nextjs_bucket_policy_public_read" {
         Effect    = "Allow"
         Principal = "*"
         Action    = "s3:GetObject"
-        Resource  = "${aws_s3_bucket.nextjs-website-bucket.arn}/*"
+        Resource  = "arn:aws:s3:::nextjs-portfolio-br-website/*"
       },
     ]
   })
   
 }
+
+# Origin access identity - allowing Cloudfront to access the bucket
+
+resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {
+  comment = "OAI access for nextjs-portfolio-br-website"
+}
 # Cloudfront distribution
 
 resource "aws_cloudfront_distribution" "cdf1" {
   origin {
-    domain_name = "bartman-websiteproject-state.s3.eu-west-2.amazonaws.com"
-    origin_id   = "S3-bartman-websiteproject-state"
+    domain_name = aws_s3_bucket.nextjs-website-bucket.bucket_regional_domain_name
+    origin_id = "s3-nextjs-portfolio-br-website"
+    
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.origin_access_identity.cloudfront_access_identity_path
+    }
   }
+    enabled = true
+    comment = "nextjs-portfolio-br-website"
+    default_root_object = "index.html"
+  
+  
 
-  enabled             = true
-  default_root_object = "index.html"
+
 
   default_cache_behavior {
-    target_origin_id       = "S3-bartman-websiteproject-state"
+  
+    target_origin_id       = "s3-nextjs-portfolio-br-website"
     viewer_protocol_policy = "redirect-to-https"
-    allowed_methods        = ["GET", "HEAD"]
+    allowed_methods        = ["GET", "HEAD", "OPTIONS"]
     cached_methods         = ["GET", "HEAD"]
-
-    forwarded_values {
+  
+  
+  forwarded_values {
       query_string = false
       cookies {
         forward = "none"
       }
-    }
+  }
 
     min_ttl     = 0
     default_ttl = 3600
